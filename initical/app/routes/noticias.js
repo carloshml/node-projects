4a3b9e5c50853b5af25462ff7c89fab138270ebc
carloshml <hen9@ymail.com>
2018-01-17 22:53:25 -0300

    Inicio da conexao com banco mysql

--- initical/app/routes/noticias.js
+++ initical/app/routes/noticias.js
@@ -1,5 +1,20 @@
 module.exports = function(app){
-    app.get('/noticias',function(req, res){
-      res.render("noticias/noticias");
+
+app.get('/noticias',function(req, res){
+  var mysql = require('mysql');
+
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'root',
+    database:'portal_noticias'
+  });
+
+  connection.query('select * from noticias', function(error, resultado){
+       res.render("noticias/noticias", {noticias: resultado });
+  });
+
+
+
     });
 }
