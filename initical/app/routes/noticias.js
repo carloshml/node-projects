b136cdf4d8ac954f75ba8a4f670059bf0575fc46
carloshml <hen9@ymail.com>
2018-01-17 22:58:08 -0300

    estruturacao da conexao com o banco

--- initical/app/routes/noticias.js
+++ initical/app/routes/noticias.js
@@ -1,14 +1,11 @@
+var dbConection = require('./../../config/db_connection')
+
 module.exports = function(app){
 
-app.get('/noticias',function(req, res){
-  var mysql = require('mysql');
+  var connection = dbConection();
 
-  var connection = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password:'root',
-    database:'portal_noticias'
-  });
+app.get('/noticias',function(req, res){
+
 
   connection.query('select * from noticias', function(error, resultado){
        res.render("noticias/noticias", {noticias: resultado });
