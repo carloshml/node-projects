e4dc445bc7b994ff8aa8fe4483bacd3a8e857f71
Carlos <hen9@ymail.com>
2024-06-29 12:09:35 -0300

    fix: refactoring

--- initical/app/controllers/noticias.js
+++ initical/app/controllers/noticias.js
@@ -1,23 +1,17 @@
-module.exports.noticias = function(application, req, res){
-  var connection = application.config.db_connection() ;
+module.exports.noticias = function (application, req, res) {
+  var connection = application.config.db_connection();
   var noticiasModel = new application.app.models.NoticiasDao(connection);
-  noticiasModel.getNoticias(function(error, resultado){
-
-       res.render("noticias/noticias", {noticias: resultado });
-
-    });
+  noticiasModel.getNoticias(function (error, resultado) {
+    res.render("noticias/noticias", { noticias: resultado });
+  });
 }
 
-module.exports.noticia = function(application, req, res){
+module.exports.noticia = function (application, req, res) {
   var connection = application.config.db_connection();
-  var noticiasModel =  new application.app.models.NoticiasDao(connection);
-
+  var noticiasModel = new application.app.models.NoticiasDao(connection);
   var id_noticia = req.query;
-  noticiasModel.getNoticia( id_noticia,  function(error, resultado){
-
+  noticiasModel.getNoticia(id_noticia, function (error, resultado) {
     console.log('---------------', error, resultado)
-
-       res.render("noticias/noticia", {noticias: resultado });
-
-    });
+    res.render("noticias/noticia", { noticias: resultado });
+  });
 }
--- initical/app/routes/noticias.js
+++ initical/app/routes/noticias.js
@@ -1,16 +1,14 @@
 //var dbConection = require('./../../config/db_connection')
 
-module.exports = function(application){
+module.exports = function (application) {
+      application.get('/noticia', function (req, res) {
+            application.app.controllers.noticias.noticia(application, req, res);
+      });
+      application.get('/noticias', function (req, res) {
+            application.app.controllers.noticias.noticias(application, req, res);
+      });
 
-
-
-application.get('/noticia',function(req, res){
-      application.app.controllers.noticias.noticia( application,req, res );
-
-});
-
-application.get('/noticias',function(req, res){
-      application.app.controllers.noticias.noticias( application,req, res );
-
-});
+      app.post('/noticias/salvar', function (req, res) {
+            app.app.controllers.admin.noticias_salvar(app, req, res);
+      });
 }
